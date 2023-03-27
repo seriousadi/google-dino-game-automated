55cf0588c18e690e6b765231d2594b053a3febad
Aditya Kumar <aaz22hsns@gmail.com>
2023-03-27 11:22:18 +0530

    Add loop to code

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ driver = webdriver.Chrome()
 game_on = True
 driver.get('https://elgoog.im/t-rex/')
 driver.implicitly_wait(5)
-#driver.execute_script("document.body.style.zoom='50%'")
-#sleep(2)
+# driver.execute_script("document.body.style.zoom='50%'")
+# sleep(2)
 driver.find_element(By.CSS_SELECTOR, 'body').send_keys(Keys.SPACE)
-driver.find_element(By.XPATH, '/html/body/div[2]/div[2]').screenshot(filename='screenshot.png')
-image = Image.open('screenshot.png')
-cropped = image.crop((80, 100, 120, 125))
-cropped_colors = cropped.convert('RGB').getcolors()
-if len(cropped_colors) > 1:
-    driver.find_element(By.CSS_SELECTOR, 'body').send_keys(Keys.SPACE)
+while game_on:
+    driver.find_element(By.XPATH, '/html/body/div[2]/div[2]').screenshot(filename='screenshot.png')
+    image = Image.open('screenshot.png')
+    cropped = image.crop((80, 100, 120, 125))
+    cropped_colors = cropped.convert('RGB').getcolors()
+    if len(cropped_colors) > 1:
+        driver.find_element(By.CSS_SELECTOR, 'body').send_keys(Keys.SPACE)
+    else:
+        pass
